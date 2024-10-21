304cca58f83930b0f3b37354e180e7ee4f4cce26
Nelly <Nelly.sosobrado@gmail.com>
2024-10-21 14:05:48 +0200

    Organising with folders

---
+++ DuckApp_refactored/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckApp_refactored
+{
+    //Context
+    //Duck class will delegate who does what
+    public class Duck
+    {
+        public IFlyBehavior FlyBehavior;
+        public IQuackBehavior quackBehavior;
+
+        public void performQuack()
+        {
+            quackBehavior.QuackSound();
+        }
+
+       
+    }
+}
